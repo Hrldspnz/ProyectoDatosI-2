ded53e622880de08cf1147d7cd170c71f8ad562d
Harold <haroldespinoza823@gmail.com>
2019-10-11 23:52:45 -0600

    quick sort
    (Falta boton)

--- Text_Finder/src/Sort/Quicksort.java
+++ Text_Finder/src/Sort/Quicksort.java
@@ -3,37 +3,39 @@ package Sort;
 
 
     public class Quicksort  {
-        private static int[] numbers;
+        private static String[] names;
         private static int number;
         
-        public static void main(String[] args){
-            int arr[] = {3,2,10,1,5};
-            sort(arr);
+        public static String[] Ordenar(String[] nombres){
+            //String arr[] = {"Marco","Fernando","Carlos","Jose","Ana","Miguel"};
+            sort(nombres);
+            printArray(names, 0,0);
+            return nombres;
         }
         
-        public static void sort(int[] values) {
+        public static void sort(String[] values) {
             // check for empty or null array
             if (values ==null || values.length==0){
                 return;
             }
-            numbers = values;
+            names = values;
             number = values.length;
             quicksort(0, number - 1);
         }
         private static void quicksort(int low, int high) {
             int i = low, j = high;
-            int pivot = numbers[low + (high-low)/2];
-            System.out.println("pivote: " + pivot);
-            System.out.println("valores iniciales de i, j "+i +" "+ j);
+            String pivot = names[low + (high-low)/2];
+            //System.out.println("pivote: " + pivot);
+            //System.out.println("valores iniciales de i, j "+i +" "+ j);
             while (i <= j) {
-                while (numbers[i] < pivot) {
+                while (names[i].compareToIgnoreCase(pivot) < 0) {
                     i++;
                 }
-                while (numbers[j] > pivot) {
+                while (names[j].compareToIgnoreCase(pivot) > 0) {
                     j--;
                 }
                 if (i <= j) {
-                    System.out.println("valores despues del bucle "+i +" "+ j);
+                    //System.out.println("valores despues del bucle "+i +" "+ j);
                     exchange(i, j);
                     i++;
                     j--;
@@ -46,14 +48,14 @@ package Sort;
         }
 
         private static void exchange(int i, int j) {
-            int temp = numbers[i];
-            numbers[i] = numbers[j];
-            numbers[j] = temp;
-            System.out.println("intercambiados: "+numbers[i] +" "+ numbers[j]);
-            printArray(numbers, 0,0);
+            String temp = names[i];
+            names[i] = names[j];
+            names[j] = temp;
+            //System.out.println("intercambiados: "+names[i] +" "+ names[j]);
+            
         }
 
-        static void printArray(int arr[], int i1, int i2){
+        static void printArray(String arr[], int i1, int i2){
             int n = arr.length;
             for (int i=0; i<n; ++i)
                 //if(i == i1)
