ec2255b58e52067bde6dc51574c35173be9b9db2
Harold <haroldespinoza823@gmail.com>
2019-10-12 21:13:50 -0600

    limpieza de codigo

--- Text_Finder/src/Sort/BubbleSort.java
+++ Text_Finder/src/Sort/BubbleSort.java
@@ -10,27 +10,27 @@ import java.util.Date;
  */
  
 public class BubbleSort {
-        public static String[] bubble_srt(String array[]) {
-            int n = array.length;
-            int k;
-            for (int m = 0; m < n; m++) {
-                for (int i = 0; i < n - 1; i++) {
-                    k = i + 1;
-                    File filedate = new File(direccion+"\\"+array[i]);
-                    File filedate2 = new File(direccion+"\\"+array[k]);
-                    Date d = new Date(filedate.lastModified());
-                    Date d2 = new Date(filedate2.lastModified());
-                    
-                    if (d.before(d2)) {
-                        String temp;
-                        temp = array[i];
-                        array[i] = array[k];
-                        array[k] = temp;
-                    }
+    public static String[] bubble_srt(String array[]) {
+        int n = array.length;
+        int k;
+        for (int m = 0; m < n; m++) {
+            for (int i = 0; i < n - 1; i++) {
+                k = i + 1;
+                File filedate = new File(direccion+"\\"+array[i]);
+                File filedate2 = new File(direccion+"\\"+array[k]);
+                Date d = new Date(filedate.lastModified());
+                Date d2 = new Date(filedate2.lastModified());
+
+                if (d.before(d2)) {
+                    String temp;
+                    temp = array[i];
+                    array[i] = array[k];
+                    array[k] = temp;
                 }
-                
-            }return array;
-            
-        }
+            }
+
+        }return array;
 
     }
+
+}
--- Text_Finder/src/Sort/Quicksort.java
+++ Text_Finder/src/Sort/Quicksort.java
@@ -1,22 +1,20 @@
 
 package Sort;
 
-/**
- * @author Harold EM
- */
+    /**
+     * @author Harold EM
+     */
     public class Quicksort  {
         private static String[] names;
         private static int number;
         
         public static String[] Ordenar(String[] nombres){
-            //String arr[] = {"Marco","Fernando","Carlos","Jose","Ana","Miguel"};
+           
             sort(nombres);
-            printArray(names, 0,0);
             return nombres;
         }
         
         public static void sort(String[] values) {
-            // check for empty or null array
             if (values ==null || values.length==0){
                 return;
             }
@@ -27,8 +25,6 @@ package Sort;
         private static void quicksort(int low, int high) {
             int i = low, j = high;
             String pivot = names[low + (high-low)/2];
-            //System.out.println("pivote: " + pivot);
-            //System.out.println("valores iniciales de i, j "+i +" "+ j);
             while (i <= j) {
                 while (names[i].compareToIgnoreCase(pivot) < 0) {
                     i++;
@@ -37,7 +33,6 @@ package Sort;
                     j--;
                 }
                 if (i <= j) {
-                    //System.out.println("valores despues del bucle "+i +" "+ j);
                     exchange(i, j);
                     i++;
                     j--;
@@ -53,15 +48,12 @@ package Sort;
             String temp = names[i];
             names[i] = names[j];
             names[j] = temp;
-            //System.out.println("intercambiados: "+names[i] +" "+ names[j]);
             
         }
 
         static void printArray(String arr[], int i1, int i2){
             int n = arr.length;
             for (int i=0; i<n; ++i)
-                //if(i == i1)
-                //    System.out.print(arr[i] + " " + i1 + " " + i2 + " ");
                 System.out.print(arr[i] + " ");
         System.out.println();
         }
