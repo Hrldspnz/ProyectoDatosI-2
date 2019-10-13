4eaa98a7fd831ec88a2f1dbaf3d9c271ddbc15f0
Harold <haroldespinoza823@gmail.com>
2019-10-12 21:05:06 -0600

    BubbleSort

--- Text_Finder/src/Sort/BubbleSort.java
+++ Text_Finder/src/Sort/BubbleSort.java
@@ -1,35 +1,36 @@
 package Sort;
 
+import static Sort.RadixSort.direccion;
+import java.io.File;
+import java.util.Date;
+
+
+/***
+ * @author HaroldEM
+ */
+ 
 public class BubbleSort {
-        public static void bubble_srt(int array[]) {
+        public static String[] bubble_srt(String array[]) {
             int n = array.length;
             int k;
             for (int m = 0; m < n; m++) {
                 for (int i = 0; i < n - 1; i++) {
                     k = i + 1;
-                    if (array[i] > array[k]) {
-                        int temp;
+                    File filedate = new File(direccion+"\\"+array[i]);
+                    File filedate2 = new File(direccion+"\\"+array[k]);
+                    Date d = new Date(filedate.lastModified());
+                    Date d2 = new Date(filedate2.lastModified());
+                    
+                    if (d.before(d2)) {
+                        String temp;
                         temp = array[i];
                         array[i] = array[k];
                         array[k] = temp;
                     }
                 }
-                printNumbers(array);
-            }
-        }
-        private static void swapNumbers(int i, int j, int[] array) {
-
+                
+            }return array;
+            
         }
 
-        private static void printNumbers(int[] input) {
-            for (int i = 0; i < input.length; i++) {
-                System.out.print(input[i] + ", ");
-            }
-            System.out.println("\n");
-        }
-
-        public static void main(String[] args) {
-            int[] input = { 4, 2, 9, 6, 23, 12, 34, 0, 1 };
-            bubble_srt(input);
-        }
     }
