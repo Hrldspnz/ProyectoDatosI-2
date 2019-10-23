dd9d3814642f4e7f855ff7df0d216ae1f6aef70d
Harold <haroldespinoza823@gmail.com>
2019-10-22 18:20:30 -0600

    Ventana busqueda

---
+++ Text_Finder/src/text_finder/VentanaBusqueda.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package text_finder;
+
+
+import java.awt.Color;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.Highlighter;
+import static text_finder.Panel1.Barra;
+import static text_finder.Panel1.modeloLista;
+
+
+ /** 
+ * Clase de la ventana de busqueda  
+ * @author Harold EM
+ */ 
+public class VentanaBusqueda extends javax.swing.JFrame {
+
+    DefaultListModel moLista;
+    /** 
+    * Crea la ventana de busqueda  
+    */ 
+    public VentanaBusqueda() {
+        initComponents();
+    }
+    /** 
+     * Metodo que resalta la palabra buscada de color verde 
+     * @param area1 Recibe el Jtextarea donde se va a resaltar la palabra 
+     * @param texto Recibe el texto a buscar 
+     */ 
+    public void resaltarpalabra(JTextArea area1, String texto) {
+        if (texto.length() >= 1) {
+            DefaultHighlighter.DefaultHighlightPainter highlightPainter = new DefaultHighlighter.DefaultHighlightPainter(Color.GREEN);
+            Highlighter h = area1.getHighlighter();
+            h.removeAllHighlights();
+            String text = area1.getText();
+            String caracteres = texto;
+            Pattern p = Pattern.compile("(?i)" + caracteres);
+            Matcher m = p.matcher(text);
+            while (m.find()) {
+                try {
+                    System.out.println(m.start() + "  " + m.end());
+                    h.addHighlight(m.start(), m.end(), highlightPainter);
+                } catch (BadLocationException ex) {
+                    Logger.getLogger(VentanaBusqueda.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        } else {
+            JOptionPane.showMessageDialog(area1, "la palabra a buscar no puede ser vacia");
+        }
+    }
+    
+    @SuppressWarnings("unchecked")
+    /** 
+     * Metodo que inicia todos los componentes de la ventanas 
+     *  
+     */                                      
+    private void initComponents() {
+        
+        jScrollPane1 = new javax.swing.JScrollPane();
+        t1 = new javax.swing.JTextArea();
+        jButton1 = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        t1.setColumns(20);
+        t1.setRows(5);
+        jScrollPane1.setViewportView(t1);
+
+        jButton1.setText("Abrir");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        
+        moLista = new DefaultListModel();
+        for(Object element : modeloLista.toArray() ){
+            
+            moLista.addElement(element.toString());
+        }
+        jList1 = new JList(moLista);
+        jScrollPane2.setViewportView(jList1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 303, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(20, 20, 20)
+                        .addComponent(jButton1)
+                        .addGap(0, 24, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 184, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton1)
+                .addGap(55, 55, 55))
+        );
+
+        pack();
+    }                        
+                       
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        // TODO add your handling code here:
+        resaltarpalabra(t1,Barra.getText());
+        
+    }                                        
+
+    //Declaracion de variables - no modificar                      
+    private javax.swing.JButton jButton1;
+    private javax.swing.JList jList1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea t1;
+    // Fin de declaracion de variables                
+}
